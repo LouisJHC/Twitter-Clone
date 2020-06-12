c059e0f3e647af3c732935559aefb7be9a64a5c2
Louis Cha <lcha@sfu.ca>
2020-06-11 18:47:25 -0700

    DB set up, server side validation of the user input, save it to the DB, and retrieve all tweets from th DB.

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('.form-data');
 const loadingSpinner = document.querySelector('.loading-spinner');
-
+const tweetList = document.querySelector('.tweet-list');
 loadingSpinner.style.display = 'none';
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -18,6 +18,40 @@ form.addEventListener('submit', (e) => {
             'content-type': 'application/json'
         },
         body: JSON.stringify( { title, content })
-    })
+    }).then(json => {
+            loadingSpinner.style.display = 'none';
+            form.style.display = '';
+            form.reset();
 
+            showTweets();
+        }
+    ); 
 })
+
+function showTweets() {
+    fetch('http://localhost:8080/tweet', {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json'
+        }
+    }).then(res => res.json()).then(tweets => {
+        tweetList.innerHTML = ''; // clear out list of tweets everytime, so I don't keep append the same data over and over again.
+        tweets.reverse(); // show the latest tweet first.
+        tweets.forEach(tweet => {
+        const div = document.createElement('div');
+        const title = document.createElement('h1');
+        const content = document.createElement('p');
+        const date = document.createElement('p');
+
+        title.textContent = tweet.title;
+        content.textContent = tweet.content;
+        date.textContent = tweet.createdAt;
+
+        div.appendChild(title);
+        div.appendChild(content);
+        div.appendChild(date);
+        
+        tweetList.appendChild(div);
+        })
+    });
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const monk = require('monk');
 
+const url = 'localhost:27017/acme'
+const db = monk(url);
+
+const tweet = db.get('tweet');
+db.then(() => {
+    console.log('connected!');
+});
 app.use(cors())
 
 // JSON body parser. Any incoming requests that have content-type as application/json will be parsed by this middleware.
@@ -12,9 +20,31 @@ app.get('/', (req, res) => {
 });
 
 app.post('/tweet', (req, res) => {
-    console.log(req.body);
+    if(isValidate(req.body)) {
+        const title = req.body.title.toString().trim();
+        const content = req.body.content.toString().trim();
+        const createdAt = new Date();
+
+        tweet.insert({title, content, createdAt}).then(tweet =>
+            res.send(tweet));
+    } else {
+        res.status(422)
+        res.json({
+            message: 'Please enter a valid title and content!'
+        })
+
+    }
 });
 
+app.get('/tweet', (req, res) => {
+    tweet.find({}).then(docs => {
+        res.json(docs);
+    });
+});
+
+function isValidate(tweet) {
+    return tweet.title !== '' && tweet.title.toString().trim() != '' && tweet.content !== '' && tweet.content.toString().trim() !== '';
+}
 const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`The server running on port ${PORT}`);
